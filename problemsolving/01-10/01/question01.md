3bef0767c6ba4f7736c8228087081b2f0d9d5dfe
michael0543 <michael05430543@gmail.com>
2019-03-04 23:57:01 +0800

    problem solving 01

---
+++ problemsolving/01-10/01/question01.md
@@ -0,0 +1,3 @@
+### 01 Solve Me First
+
+<img src='../images/01.png' width='800' height='500'>
---
+++ problemsolving/01-10/01/solution01.py
@@ -0,0 +1,10 @@
+def solution01(a, b):
+    '''
+    Args:
+      a: number
+      b: number
+    Returns:
+      a + b
+    '''
+
+    return (a+b)
