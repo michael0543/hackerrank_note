b048077c2093cd2455c6c1951343f977b6ffb56d
michael0543 <michael05430543@gmail.com>
2019-03-06 00:24:13 +0800

    problem solving 04

---
+++ problemsolving/01-10/04/question04.md
@@ -0,0 +1,5 @@
+### 04 A Very Big Sum
+
+<img src='../images/04-1.png' width='800' height='500'>
+
+<img src='../images/04-2.png' width='800' height='500'>
---
+++ problemsolving/01-10/04/solution04.py
@@ -0,0 +1,9 @@
+def solution04(arr):
+    '''
+    Args:
+      arr: an array of big numbers
+    Returns:
+      sum of the array
+    '''
+
+    return sum(arr)
